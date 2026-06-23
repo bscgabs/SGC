14dab68ce6e9cf471d16393ac529683c3cc583e9
Gabriel Bernardes <157903020+bscgabs@users.noreply.github.com>
2026-06-23 20:48:58 -0300

    Delete Projeto Web - SGC/src/main directory

--- Projeto Web - SGC/src/main/java/web/sgc/repository/campeonatoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.campeonato.repository;
-
-import br.com.campeonato.model.Equipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-
-}
