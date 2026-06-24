91982061fd31b1367bf9af88f7a3000aefd7cbc8
Gabriel Bernardes <157903020+bscgabs@users.noreply.github.com>
2026-06-23 21:40:00 -0300

    Delete src/main directory

--- src/main/java/web/sgc/repository/EquipeRepository.java
+++
@@ -1,8 +0,0 @@
-package web.sgc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.sgc.model.Equipe;
-
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-
-}
